601e5444d3df94bbcda7bf1112592035f94cec5c
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-03 23:02:15 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,33 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        background: "#080808"
+      }}
+    >
+      <h1
+        style={{
+          color: "#D4AF37",
+          fontSize: "64px",
+          fontWeight: "bold"
+        }}
+      >
+        LUXURY SPINNER
+      </h1>
 
+      <p
+        style={{
+          color: "#ffffff",
+          fontSize: "20px"
+        }}
+      >
+        Website berhasil deploy di Railway
+      </p>
+    </main>
+  );
+}
