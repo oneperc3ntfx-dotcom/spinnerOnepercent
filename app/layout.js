d86d31923c955a3cec47ffa3637fdc13c93bca3d
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-03 23:58:09 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0 }}>
+      <body style={{ margin: 0, background: "#000" }}>
         {children}
       </body>
     </html>
