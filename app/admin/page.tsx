098f2a5d8b98248f542e87c33ad5d7c2378512bc
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-03 22:39:29 +0700

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,22 @@
+export default function Admin(){
+
+ return(
+
+  <div className="p-10">
+
+   <h1 className="text-4xl gold">
+    Admin Panel
+   </h1>
+
+   <div className="luxury-card p-6 mt-6">
+
+    <input
+     className="w-full p-3 text-black"
+     placeholder="Header Title"
+    />
+
+   </div>
+
+  </div>
+ );
+}
