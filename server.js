87277d3659ec95b107df1700120f4c5be4823628
oneperc3ntfx-dotcom <oneperc3ntfx@gmail.com>
2026-06-04 00:35:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Server Running");
-});
+app.listen(PORT, () => console.log("RUNNING"));
